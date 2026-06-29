a3ee754ddc5e54db0ddf3e7d49879a5b37eb268e
Arunkumar G Bhat <art.arunbhat@gmail.com>
2026-06-29 09:50:38 +0530

    added switches to the main code.

--- sma.ps1
+++ sma.ps1
@@ -3,13 +3,64 @@ $action = $args[1]
 
 switch ($module) {
 	"asset" {
-		Write-Host "Asset module selected"
+		switch ($action){
+			"add"{
+				Write-Host "Adding asset ..."
+			}
+			"list"{
+				Write-Host "Listing asset ..."
+			}
+			"delete"{
+				Write-Host "Deleting asset ..."
+			}
+			"modify"{
+				Write-Host "Modifying asset ..."
+			}
+			default{
+				Write-Host "Action not defined!"
+			}
+
+		}
 	}
 	"vendor" {
-		Write-Host "Vendor module selected"
+		switch ($action){
+			"add"{
+				Write-Host "Adding vendor ..."
+			}
+			"list"{
+				Write-Host "Listing vendor ..."
+			}
+			"delete"{
+				Write-Host "Deleting vendor ..."
+			}
+			"modify"{
+				Write-Host "Modifying vendor ..."
+			}
+			default{
+				Write-Host "Action not defined!"
+			}
+		}
 	}
+
 	"software" {
-		Write-Host "Software module selected"
+		switch ($action){
+			"add"{
+				Write-Host "Adding software ..."
+			}
+			"list"{
+				Write-Host "Listing software ..."
+			}
+			"delete"{
+				Write-Host "Deleting software ..."
+			}
+			"modify"{
+				Write-Host "Modifying software ..."
+			}
+			default{
+				Write-Host "Action not defined!"
+			}
+		}
+
 	}
 	default {
 		Write-Host "Correct Module not selected"
